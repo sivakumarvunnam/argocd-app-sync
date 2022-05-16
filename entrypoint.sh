bb26741a7f07a5f402be829b48e384fab97c04dc
Sivakumar Vunnam <sivakumarvunnam1@gmail.com>
2022-05-16 17:29:46 -0500

    check url status condition

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,17 @@ echo "==> Fetching argocd cli..."
 curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/${ARGOCD_CLI_SERVER}/download/argocd-linux-amd64
 chmod +x /usr/local/bin/argocd
 
+HTTP_CODE=$(curl --write-out "%{http_code}\n" "https://${ARGOCD_SERVER}" --output output.txt --silent)
+echo "==> ArgoCD server status code :" $HTTP_CODE
+
+if [ -z $HTTP_CODE && $HTTP_CODE == 200  ]; then
+    echo "==> Login to ArgoCD UI..."
+    argocd login $ARGOCD_SERVER --username admin --password $ARGOCD_AUTH_TOKEN --insecure
+else
+    echo "==> ArgoCD endpoint is not available"
+    exit 0
+fi
+
 echo "==> Syncing app..."
 argocd app sync $1
 
@@ -13,5 +24,5 @@ echo "==> Waiting for app to become healthy..."
 argocd app wait $1
 
 echo "==> Generating app env url output..."
-env_url=$(argocd app get $1 -o json | jq -r '.status.summary.externalURLs | sort_by(length) | .[0]')
-echo "::set-output name=env_url::${env_url}"
+#env_url=$(argocd app get $1 -o json | jq -r '.status.summary.externalURLs | sort_by(length) | .[0]')
+#echo "::set-output name=env_url::${env_url}"
