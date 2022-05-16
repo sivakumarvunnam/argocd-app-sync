8ad2d2a8d57b91e8aec6b917dccf238ad026f8de
Sivakumar Vunnam <sivakumarvunnam1@gmail.com>
2022-05-16 00:54:49 -0500

    startup script

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -l
+
+set -e
+
+echo "==> Fetching argocd cli..."
+curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/${ARGOCD_CLI_SERVER}/download/argocd-linux-amd64
+chmod +x /usr/local/bin/argocd
+
+echo "==> Syncing app..."
+argocd app sync $1
+
+echo "==> Waiting for app to become healthy..."
+argocd app wait $1
+
+echo "==> Generating app env url output..."
+env_url=$(argocd app get $1 -o json | jq -r '.status.summary.externalURLs | sort_by(length) | .[0]')
+echo "::set-output name=env_url::${env_url}"
